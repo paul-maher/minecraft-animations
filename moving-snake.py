f8f7ee12c9455e81937a54a10a630338f82c4fec
paul-maher <paul.maher@open.ac.uk>
2019-06-13 11:07:39 +0000

    Fixed some Python 3 compatiblilities

--- moving-snake.py
+++ moving-snake.py
@@ -4,6 +4,7 @@ import mcpi.block as block
 from random import randint 
 import time 
 
+# A class to implment a moving snake in Minecraft
 class Snake:
    
     # Make a snake.... make it high in the air to avoid getting tanlged up
@@ -14,31 +15,33 @@ class Snake:
         _self.body = bodyBlock
         
     def runner(_self):
-        
-        # Draw the head, 
-        _self.setBlock(_self.bits[0],_self.head)
 
-        # wait a bit
-        time.sleep(0.5)
+        while(True):
 
-        # Make it back to a body part
-        _self.setBlock(_self.bits[0],_self.body)
+	    # Draw the head, 
+            mc.setBlock(_self.bits[0],_self.head)
+
+            # wait a bit
+            time.sleep(0.5)
+
+            # Make it back to a body part
+            mc.setBlock(_self.bits[0],_self.body)
         
-        # work out the new head, add it on the end of the Q, clear the tail and remove from Q
-        _self.bits.appendleft(_self.workOutNewHeadPosition(_self.bits[0]))
+            # work out the new head, add it on the end of the Q, clear the tail and remove from Q
+            _self.bits.appendleft(_self.workOutNewHeadPosition(_self.bits[0]))
         
-        # Erase the tail
-        _self.setBlock(_self.bits.pop(),0)
+            # Erase the tail
+            mc.setBlock(_self.bits.pop(),0)
         
-    def setBlock(_self,position,block):
-        print(*position,block)
+    #def setBlock(_self,position,block):
+    #    print(*position,block)
         
-    def workOutNewHeadPosition(_self,pos):
+    def workOutNewHeadPositionOld(_self,pos):
         x,y,z = pos
         x = x + 1
         return((x,y,z))
     
-    def workOutNewHeadPositionEx(pos):
+    def workOutNewHeadPosition(_self, pos):
 
         #print "Work out new head position", headposition print "map:", map[1][0][0] if 
         #map[snake[position][0]][0] == 0:
@@ -47,25 +50,28 @@ class Snake:
         #snake[tailposition] = snake[headposition][0]+1, snake[headposition][1], snake[headposition][2]
         x,y,z = pos
         # Try to move down first of all
-        print mc.getBlock(x, y-1, z)
-        if mc.getBlock(x, y-1, z) == block.AIR:
+        print("Air is ",block.AIR)
+        print("Looking Down: ",mc.getBlock(x, y-1, z))
+        if mc.getBlock(x, y-1, z) == block.AIR.id:
+        #if mc.getBlock(x, y-1, z) == 0:
             y = y-1
         else:
+            print("Nope, not down")
             # Check out the left, right, forward, backward options
             dirList = []
-            if mc.getBlock(x+1, y, z) == block.AIR:
+            if mc.getBlock(x+1, y, z) == block.AIR.id:
                 dirList.append(1)
-            if mc.getBlock(x-1, y, z) == block.AIR:
+            if mc.getBlock(x-1, y, z) == block.AIR.id:
                 dirList.append(2)
-            if mc.getBlock(x, y, z+1) == block.AIR:
+            if mc.getBlock(x, y, z+1) == block.AIR.id:
                 dirList.append(3)
-            if mc.getBlock(x, y, z-1) == block.AIR:
+            if mc.getBlock(x, y, z-1) == block.AIR.id:
                 dirList.append(4)
-            print dirList
-            print len(dirList)
+            print(dirList)
+            print(len(dirList))
             if len(dirList) > 0:
                 selection = dirList[randint(0,len(dirList)-1)]
-                print "Select: ", selection
+                print("Select: ", selection)
                 if selection == 1:
                     x = x + 1
                 if selection == 2:
@@ -77,18 +83,22 @@ class Snake:
             else:
                 # Last resort is to go up else if getblock(x, y+1, z)
                 y = y+1
-                print "had to go up"
+                print("had to go up")
         return(x,y,z)
         
 # Connect to the game
 mc = minecraft.Minecraft.create() 
 xPos,yPos,zPos = mc.player.getTilePos() 
 
-snake = Snake(xPos,20,zPos,5,block.STONE,block.MARBLE)
+#print(type(block.AIR.id))
+#print(mc.getBlock(50,50,50))
+#sfsafsdf
+
+snake = Snake(xPos,20,zPos,5,block.STONE,block.DIRT)
 snake.runner()
-print(snake.bits)
-snake.runner()
-print(snake.bits)
-snake.runner()
-print(snake.bits)
+#print(snake.bits)
+#snake.runner()
+#print(snake.bits)
+#snake.runner()
+#print(snake.bits)
 
