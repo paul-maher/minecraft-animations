26ebaf18eaea77e63a8729b6dcc09fcb28059262
paul-maher <paul.maher@open.ac.uk>
2019-06-14 09:59:54 +0000

    Added a bias so the beast tends to try and keep moving in the same direction

--- moving-snake.py
+++ moving-snake.py
@@ -4,6 +4,13 @@ import mcpi.block as block
 from random import randint 
 import time 
 
+NORTH = 1
+SOUTH = 2
+EAST = 3
+WEST = 4
+
+BIAS = 20
+
 # A class to implment a moving snake in Minecraft
 class Snake:
    
@@ -13,6 +20,7 @@ class Snake:
         _self.bits = deque([(xPos,_,zPos) for _ in range(yPos,yPos+length)])
         _self.head = headBlock
         _self.body = bodyBlock
+        _self.selection = 0
 
     # Animate the snakes body        
     def runner(_self):
@@ -37,14 +45,7 @@ class Snake:
             # Erase the tail
             mc.setBlock(_self.bits.pop(),0)
         
-    #def setBlock(_self,position,block):
-    #    print(*position,block)
-        
-    def workOutNewHeadPositionOld(_self,pos):
-        x,y,z = pos
-        x = x + 1
-        return((x,y,z))
-    
+    # Work out the direction of movment for the next head position
     def workOutNewHeadPosition(_self, pos):
 
         x,y,z = pos
@@ -53,29 +54,40 @@ class Snake:
         if mc.getBlock(x, y-1, z) == block.AIR.id:
             y = y-1
         else:
+
             # Check out the left, right, forward, backward options
             dirList = []
             if mc.getBlock(x+1, y, z) == block.AIR.id:
-                dirList.append(1)
+                dirList.append(NORTH)
             if mc.getBlock(x-1, y, z) == block.AIR.id:
-                dirList.append(2)
+                dirList.append(SOUTH)
             if mc.getBlock(x, y, z+1) == block.AIR.id:
-                dirList.append(3)
+                dirList.append(EAST)
             if mc.getBlock(x, y, z-1) == block.AIR.id:
-                dirList.append(4)
+                dirList.append(WEST)
+
+            # Add a bias here to try and keep going in the same direction as last time if possible. Add
+            # the same option in as many times as appropriate to suit the bias level
+            if _self.selection in dirList:
+                for _ in range(BIAS):
+                    dirList.append(_self.selection)
+
+            # Now pick an option from the list at random
             if len(dirList) > 0:
-                selection = dirList[randint(0,len(dirList)-1)]
-                if selection == 1:
+                _self.selection = dirList[randint(0,len(dirList)-1)]
+                if _self.selection == NORTH:
                     x = x + 1
-                if selection == 2:
+                if _self.selection == SOUTH:
                     x = x - 1
-                if selection == 3:
+                if _self.selection == EAST:
                     z = z + 1
-                if selection == 4:
+                if _self.selection == WEST:
                     z = z - 1
             else:
-                # Last resort is to go up else if getblock(x, y+1, z)
+                # If there are no options, last resort is to go up. This will overwrite blocks
                 y = y+1
+
+        # And return the new position  
         return(x,y,z)
         
 # Connect to the game
@@ -83,6 +95,6 @@ mc = minecraft.Minecraft.create()
 xPos,yPos,zPos = mc.player.getTilePos() 
 
 # Make a snake and animate it
-snake = Snake(xPos,30,zPos,50,block.LAPIS_LAZULI_ORE,block.LAPIS_LAZULI_BLOCK)
+snake = Snake(xPos,30,zPos,10,block.LAPIS_LAZULI_ORE,block.LAPIS_LAZULI_BLOCK)
 snake.runner()
 
