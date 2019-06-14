7ed425a3807fd0ac4cfb4f67daea490270437691
paul-maher <paul.maher@open.ac.uk>
2019-06-14 10:50:49 +0000

    Added comments and removed legacy method

--- moving-snake.py
+++ moving-snake.py
@@ -4,13 +4,16 @@ import mcpi.block as block
 from random import randint 
 import time 
 
+# Define directions
 NORTH = 1
 SOUTH = 2
 EAST = 3
 WEST = 4
 
+# Tendancy to keep moving in the same direction. 
 BIAS = 20
 
+# Blocks that are considered 'passable' If you don't want to go under water remove from this list
 passable = [block.AIR.id,block.WATER_STATIONARY.id]
 
 # A class to implment a moving snake in Minecraft
@@ -37,7 +40,7 @@ class Snake:
             time.sleep(0.5)
 
             # work out the new head position
-            newPos = _self.workOutNewHeadPositionEx(_self.bits[0])
+            newPos = _self.workOutNewHeadPosition(_self.bits[0])
 
             # Make the head back to a body part
             mc.setBlock(_self.bits[0],_self.body)        
@@ -49,50 +52,6 @@ class Snake:
         
     # Work out the direction of movment for the next head position
     def workOutNewHeadPosition(_self, pos):
-
-        x,y,z = pos
-
-        # Try to move down first of all
-        if mc.getBlock(x, y-1, z) == block.AIR.id:
-            y = y-1
-        else:
-
-            # Check out the left, right, forward, backward options
-            dirList = []
-            if mc.getBlock(x+1, y, z) == block.AIR.id:
-                dirList.append(NORTH)
-            if mc.getBlock(x-1, y, z) == block.AIR.id:
-                dirList.append(SOUTH)
-            if mc.getBlock(x, y, z+1) == block.AIR.id:
-                dirList.append(EAST)
-            if mc.getBlock(x, y, z-1) == block.AIR.id:
-                dirList.append(WEST)
-
-            # Add a bias here to try and keep going in the same direction as last time if possible. Add
-            # the same option in as many times as appropriate to suit the bias level
-            if _self.selection in dirList:
-                for _ in range(BIAS):
-                    dirList.append(_self.selection)
-
-            # Now pick an option from the list at random
-            if len(dirList) > 0:
-                _self.selection = dirList[randint(0,len(dirList)-1)]
-                if _self.selection == NORTH:
-                    x = x + 1
-                if _self.selection == SOUTH:
-                    x = x - 1
-                if _self.selection == EAST:
-                    z = z + 1
-                if _self.selection == WEST:
-                    z = z - 1
-            else:
-                # If there are no options, last resort is to go up. This will overwrite blocks
-                y = y+1
-
-        # And return the new position  
-        return(x,y,z)
-
-    def workOutNewHeadPositionEx(_self, pos):
 
         x,y,z = pos
 
